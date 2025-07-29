796450104628872d9b638564824df02a3cc7f656
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-07-29 21:08:15 +0200

    Initial Vite+React scaffold

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-green-400">
+      <h1>Annoying popup here</h1>
+    </div>
+  )
+}
+
+export default App
